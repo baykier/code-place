2f13e6f82a2279408839b6a5bbf595f7ce551698
zhangzhijie <zhangzhijie@wanglibank.com>
2018-09-28 08:02:05 +0000

    pop pkg added

---
+++ golang/gopl/pkg/pop/pop.go
@@ -0,0 +1,22 @@
+//package pop
+
+package pop
+
+var pc[256]byte
+
+func init () {
+	for i, _ := range pc {
+		pc[i] = pc[i/2] + pc[i&1]
+	}
+}
+
+func Count(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
