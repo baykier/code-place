2a64586b247bdfcf131ff5dffb1077fc35f4fd6c
zhangzhijie <zhangzhijie@wanglibank.com>
2018-09-28 02:49:52 +0000

    bug fixed

--- golang/gopl/pkg/temp/celsius.go
+++ golang/gopl/pkg/temp/celsius.go
@@ -7,21 +7,6 @@ import (
 
 type Celsius float64 //摄氏温度
 
-type Fahrenheit float64 //华氏温度
-
-
-
-//摄氏度转换为华氏温度
-func CToF(c Celsius) Fahrenheit {
-	return Fahrenheit(c*9/5 + 32)
-}
-
-//华氏温度转为摄氏温度
-
-func FToC(f Fahrenheit) Celsius {
-	return Celsius((f - 32) * 5 / 9)
-}
-
 func (c Celsius) String() string {
 	return fmt.Sprintf("%g°C", c)
 }
